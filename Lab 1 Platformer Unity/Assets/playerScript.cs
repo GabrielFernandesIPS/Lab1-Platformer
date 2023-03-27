c21eb71a2b978e20ea47e0a55dabe9b9f7f67194
Gabriel Fernandes <202200106@estudantes.ips.pt>
2023-03-27 12:14:31 +0100

    Movimento do Jogador

---
+++ Lab 1 Platformer Unity/Assets/playerScript.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerScript : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    [SerializeField] private bool jumpCommand;   
+    [SerializeField] private float jumpPower;
+    [SerializeField] private float runSpeed;
+    private bool leftCommand;
+    private bool rightCommand;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        jumpPower = 2.0f;
+        runSpeed = 1.0f;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jumpCommand = true;
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            leftCommand = true;
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            rightCommand = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (jumpCommand)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+            jumpCommand = false;    
+        }
+        if (leftCommand)
+        {
+            rb.velocity = new Vector2(-runSpeed, rb.velocity.y);
+            leftCommand = false;
+        }
+        if (rightCommand)
+        {
+            rb.velocity = new Vector2(runSpeed, rb.velocity.y);
+            rightCommand = false;
+        }
+    }
+}
