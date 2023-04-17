631b99ba8a1199f2ff573d83efcba034d31f4945
Gabriel Fernandes <202200106@estudantes.ips.pt>
2023-04-17 12:14:16 +0100

    Script EB atualizado

--- Lab 1 Platformer Unity/Assets/EnemyBehaviour.cs
+++ Lab 1 Platformer Unity/Assets/EnemyBehaviour.cs
@@ -4,24 +4,26 @@ using UnityEngine;
 
 public class EnemyBehaviour : MonoBehaviour
 {
-    [SerializeField] private Vector3 _enemySpeed;
+    [SerializeField] private float _enemySpeed;
     [SerializeField] private float _enemyLife;
     [SerializeField] private GameObject _enemyBullet;
+    private Rigidbody2D _enemyRb;
+
 
     void Start()
     {
-        
+        _enemyRb = GetComponent<Rigidbody2D>();        
     }
 
     
     void Update()
     {
-        
+        EnemyChase();
     }
 
     private void EnemyChase()
     {
-        transform.Translate(_enemySpeed * Time.deltaTime);
+        _enemyRb.velocity = new Vector3(_enemySpeed, _enemyRb.velocity.y);
     }
     private void EnemyAttack()
     {
